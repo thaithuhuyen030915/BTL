2dae0dc171e60574c7d372feb7fa0f0319bb28fb
w3schools-test <test@w3schools.com>
2024-05-18 21:35:40 +0700

    thêm label

--- BTL/frmBaocao.Designer.cs
+++ BTL/frmBaocao.Designer.cs
@@ -29,20 +29,34 @@ namespace BTL
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(190, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BÁO CÁO DOANH THU";
             // 
             // frmBaocao
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Name = "frmBaocao";
             this.Text = "Báo cáo";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
