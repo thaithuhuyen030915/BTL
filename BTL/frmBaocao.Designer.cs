80c3a339f98286bd4f5e0650491e699fb889a0f9
w3schools-test <test@w3schools.com>
2024-05-18 21:30:53 +0700

    commit

--- BTL/frmBaocao.Designer.cs
+++ BTL/frmBaocao.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace BTL
 {
-    partial class Form1
+    partial class frmBaocao
     {
         /// <summary>
         /// Required designer variable.
@@ -29,10 +29,17 @@ namespace BTL
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmBaocao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "frmBaocao";
+            this.Text = "Báo cáo";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- BTL/frmBaocao.cs
+++ BTL/frmBaocao.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace BTL
 {
-    public partial class Form1 : Form
+    public partial class frmBaocao : Form
     {
-        public Form1()
+        public frmBaocao()
         {
             InitializeComponent();
         }
